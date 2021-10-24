c5d96d0141cb6a1c222304c3c759acd5bebc19de
bitf18a501 <bitf18a501@pucit.edu.pk>
2021-10-24 11:37:40 +0500

    root letter checked

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -25,8 +25,139 @@ public class MainActivity extends AppCompatActivity {
         if (compareOneTwo == 0)
         {
             TextView text = (TextView) findViewById(R.id.textView2);
-            text.setText("Given character is a Base Character");
+            text.setText("Given character is a grass Character");
 
+        }
+
+        else
+        {
+            int comparec = Character.compare(char1, 'c');
+            if (comparec == 0)
+            {
+                TextView text = (TextView) findViewById(R.id.textView2);
+                text.setText("Given character is a grass Character");
+
+            }
+            else
+            {
+                int comparee = Character.compare(char1, 'e');
+                if (comparee == 0)
+                {
+                    TextView text = (TextView) findViewById(R.id.textView2);
+                    text.setText("Given character is a grass Character");
+
+                }
+                else
+                {
+                    int comparei = Character.compare(char1, 'i');
+                    if (comparei == 0)
+                    {
+                        TextView text = (TextView) findViewById(R.id.textView2);
+                        text.setText("Given character is a grass Character");
+
+                    }
+                    else
+                    {
+                        int comparem = Character.compare(char1, 'm');
+                        if (comparem == 0)
+                        {
+                            TextView text = (TextView) findViewById(R.id.textView2);
+                            text.setText("Given character is a grass Character");
+
+                        }
+                        else
+                        {
+                            int comparen = Character.compare(char1, 'n');
+                            if (comparen == 0)
+                            {
+                                TextView text = (TextView) findViewById(R.id.textView2);
+                                text.setText("Given character is a grass Character");
+
+                            }
+                            else
+                            {
+                                int compareo = Character.compare(char1, 'o');
+                                if (compareo == 0)
+                                {
+                                    TextView text = (TextView) findViewById(R.id.textView2);
+                                    text.setText("Given character is a grass Character");
+
+                                }
+                                else
+                                {
+                                    int comparer = Character.compare(char1, 'r');
+                                    if (comparer == 0)
+                                    {
+                                        TextView text = (TextView) findViewById(R.id.textView2);
+                                        text.setText("Given character is a grass Character");
+
+                                    }
+                                    else
+                                    {
+                                        int compares = Character.compare(char1, 's');
+                                        if (compares == 0)
+                                        {
+                                            TextView text = (TextView) findViewById(R.id.textView2);
+                                            text.setText("Given character is a root Character");
+
+                                        }
+                                        else
+                                        {
+                                            int compareu = Character.compare(char1, 'u');
+                                            if (compareu == 0)
+                                            {
+                                                TextView text = (TextView) findViewById(R.id.textView2);
+                                                text.setText("Given character is a grass Character");
+
+                                            }
+                                            else
+                                            {
+                                                int comparev = Character.compare(char1, 'v');
+                                                if (comparev == 0)
+                                                {
+                                                    TextView text = (TextView) findViewById(R.id.textView2);
+                                                    text.setText("Given character is a grass Character");
+
+                                                }
+                                                else
+                                                {
+                                                    int comparew = Character.compare(char1, 'w');
+                                                    if (comparew == 0)
+                                                    {
+                                                        TextView text = (TextView) findViewById(R.id.textView2);
+                                                        text.setText("Given character is a grass Character");
+
+                                                    }
+                                                    else
+                                                    {
+                                                        int comparex = Character.compare(char1, 'x');
+                                                        if (comparex == 0)
+                                                        {
+                                                            TextView text = (TextView) findViewById(R.id.textView2);
+                                                            text.setText("Given character is a grass Character");
+
+                                                        }
+                                                        else
+                                                        {
+                                                            int comparez = Character.compare(char1, 'z');
+                                                            if (comparez == 0)
+                                                            {
+                                                                TextView text = (TextView) findViewById(R.id.textView2);
+                                                                text.setText("Given character is a grass Character");
+
+                                                            }
+                                                        }
+                                                    }
+                                                }
+                                            }
+                                        }
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            }
         }
 
     }
